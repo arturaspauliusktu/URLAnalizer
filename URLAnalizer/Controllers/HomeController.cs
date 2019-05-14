86e9e87f129799696b4af869e06d74291152a407
Tadas Kalvaitis <t.kalvaitis@ktu.edu>
2019-05-14 16:11:08 +0300

    ReadData

--- URLAnalizer/Controllers/HomeController.cs
+++ URLAnalizer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,8 +11,18 @@ namespace URLAnalizer.Controllers
 {
     public class HomeController : Controller
     {
+        public List<URLData> DataFromFile = new List<URLData>();
+
+
         public IActionResult Index()
         {
+            if (DataFromFile.Count == 0)
+                ReadDataFromFile();
+
+            foreach(URLData d in DataFromFile)
+                Console.WriteLine("Reultatas" + d.Result);
+
+
             return View();
         }
 
@@ -39,5 +50,31 @@ namespace URLAnalizer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+       //--------------------------------------------------------MUSU KODAS----------------------------------------------------------------------------
+
+        public void ReadDataFromFile()
+        {
+            bool firstLine = true;
+
+
+            string[] lines = System.IO.File.ReadAllLines("~/../Data/dataset.txt");
+            foreach(string line in lines)
+            {
+                if (firstLine)
+                {
+                    firstLine = false;
+                    continue;
+                }
+
+                string[] s = line.Split('\t');
+                DataFromFile.Add(new URLData(int.Parse(s[1]), int.Parse(s[2]), int.Parse(s[3]), int.Parse(s[4]), int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), int.Parse(s[8]),
+                    int.Parse(s[9]), int.Parse(s[10]), int.Parse(s[11])));
+            }
+        }
+
+
+
     }
 }
